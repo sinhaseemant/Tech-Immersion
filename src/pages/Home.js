887fe1a4c5b99dcb615de7561bbde41881e21d73
Rahul Kumaresan <rahul.kumaresan@providence.org>
2024-07-14 19:56:27 +0530

    styling changes for login and create account

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Image from "../assets/image.png";
+import Logo from "../assets/logo.png";
+import { FaEye } from "react-icons/fa6";
+import { FaEyeSlash } from "react-icons/fa6";
 
 const Home=()=> {
 const navigate = useNavigate();
 
-
+const [ showPassword, setShowPassword ] = useState(false);
 
   function gotoLocatePage(){
     ValidateUser();
     navigate("/locate");
   }
-  function gotoAdminLoginPage(){
-    navigate("/adminLogin");
-  }
   function gotoCreatePage(){
     navigate("/createAcc");
+  }
+  function gotoCrudPage(){
+    navigate("/crud");
   }
 
   function ValidateUser(userName="tesuser1",password="test123"){
@@ -44,20 +48,44 @@ fetch("http://localhost:7071/api/validateUser", requestOptions)
     
         console.log("Home is called");
       return (
-        <header className="Header">
 
-          <h2>Hospital Locator</h2>
- <div>
- <input type="text" placeholder='UserName'></input>
- </div><div>
- <input type="password" placeholder='Password'></input>
- </div><div>
- <button onClick={gotoLocatePage}>Login</button>
- <button onClick={gotoAdminLoginPage}>Admin Login</button>
- </div><div>
- <button onClick={gotoCreatePage}>Create an Account</button>
- </div>
-        </header>
+<div className="login-signup-main">
+      <div className="login-signup-left">
+        <img src={Image} alt="" />
+      </div>
+      <div className="login-signup-right">
+        <div className="login-signup-right-container">
+          <div className="login-signup-logo">
+            <img src={Logo} alt="" />
+          </div>
+          <div className="login-signup-center">
+            <h2>Hospital Locator</h2>
+            <p>Login</p>
+            <form>
+              <input type="email" placeholder="Email" />
+              <div className="pass-input-div">
+                <input type={showPassword ? "text" : "password"} placeholder="Password" />
+                {showPassword ? <FaEyeSlash onClick={() => {setShowPassword(!showPassword)}} /> : <FaEye onClick={() => {setShowPassword(!showPassword)}} />}
+                
+              </div>
+
+              
+              <div className="login-signup-center-buttons">
+                <button onClick={gotoLocatePage} type="button">Log In</button>
+                <button onClick={gotoCrudPage} type="button">Admin Log In</button>
+              </div>
+            </form>
+          </div>
+
+          <p className="login-signup-bottom-p">
+            Don't have an account? <a href="#" onClick={gotoCreatePage}>Sign Up</a>
+          </p>
+        </div>
+      </div>
+    </div>
+
+
+
       );
     }
     
